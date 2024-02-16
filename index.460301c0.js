8e99df3bce2ac1260a41b9a2e814449208a172fd
sheludkovladyslav <128414119+sheludkovladyslav@users.noreply.github.com>
2024-02-16 18:31:08 +0000

    Deploying to gh-pages from @ sheludkovladyslav/Britlex@d289d949022fe28b8e25d2d97db2b5abed85c0f3 🚀

---
+++ index.460301c0.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-open_btn]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){t.classList.toggle("menu-open"),document.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.460301c0.js.map
