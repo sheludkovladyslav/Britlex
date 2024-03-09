86f09155a620a9e912ad403e2c0b7cb6aa9dbbb6
sheludkovladyslav <128414119+sheludkovladyslav@users.noreply.github.com>
2024-03-09 17:53:42 +0000

    Deploying to gh-pages from @ sheludkovladyslav/Britlex@e64530c53659705ca2f714f22c04219a7de258b7 🚀

---
+++ index.b0bd0a0f.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var o=document.getElementById("to-top");window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.classList.add("scrolled"):o.classList.remove("scrolled")}}));
+//# sourceMappingURL=index.b0bd0a0f.js.map
